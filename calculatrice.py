9aa76dadd9113670970c261939f48f58ed80c9e3
kakone <39957376+joekakone@users.noreply.github.com>
2019-06-07 08:48:06 +0000

    Ajout du fichier calculatrice.py qui fait appel à la fonction calc du module mes_fonctions

---
+++ calculatrice.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env pyton3
+# coding : utf-8
+
+# Importation de calc
+from mes_fonctions import calc
+
+def main():
+	while True:
+		calc()
+		reponse = ""
+		while reponse not in ["O", "N"]:
+			reponse = input("Voulez-vous continuer? (O/N)\nRéponse: ").upper()
+
+		if reponse == "N":
+			break
+
+	print("Merci d'utilser CALCULATRICE SIMPLE")
+
+if __name__ == "__main__":
+	main()
